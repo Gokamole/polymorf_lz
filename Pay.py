d07d0716093d8192cfa1184139a200b86c8e3032
Gokamole <gokamole@tutamail.com>
2025-04-21 16:30:34 +0300

    3

--- Pay.py
+++ Pay.py
@@ -4,9 +4,18 @@ class Payment:
         self.file_name = file_name
         self.df = pd.read_csv(file_name)
 
-    def copy(self):
-        key = ['Участники гражданского оборота','Тип операции','Сумма операции','Вид расчета','Место оплаты','Терминал оплаты','Дата оплаты','Время оплаты','Результат операции','Cash-back','Сумма cash-back']
-        self.df_dedupped2 = self.df.drop_duplicates(subset=key)
+    def __pos__(self):
+        initial_count = len(self.data)
+        self.data = self.data.drop_duplicates()
+        removed_count = initial_count - len(self.data)
+        print(f"Количество повторяющихся строк в наборе данных: {removed_count}")
         
+    def pay_time(self):
+        self.df['Дата оплаты'] = pd.to_datetime(self.df['Дата оплаты'])
+        self.df['Год'] = self.df['Дата оплаты'].df.year
+        self.data_before = self.df[self.df['year_numeric'] < 2014]
+        self.data_after = self.df[self.df['year_numeric'] >= 2014]
+        self.data_before.to_csv("data_before_2014.csv", index=False)
+        self.data_after.to_csv("data_after_2014.csv", index=False)
         
 file_name = "var7.csv"
